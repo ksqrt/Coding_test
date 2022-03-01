b0619b7c195a1b131f01dbe21b85b355ff45f3f4
ksqrt <ksqrt98@gmail.com>
2022-03-01 17:20:34 +0900

    #17298

--- 백준기초1/test.py
+++ 백준기초1/test.py
@@ -1,43 +1,22 @@
-#10845
+#17298 오큰수
 import sys
-input=sys.stdin.readline
 
-stack1 = []
+n = int(sys.stdin.readline())
+arr1 = [list(map(int,sys.stdin.readline().split())) for _ in range(n)]
 
-def stack_util(commend):
-    if commend[0] == "push":
-        stack1.insert(0,int(commend[1]))
-        return
-    elif commend[0] == "pop":
-        if len(stack1)==0:
-            print(-1)
-        else:
-            print(stack1.pop())
-        return
-    elif commend[0] == "size":
-        print(len(stack1))
-        return
-    elif commend[0] == "empty":
-        if len(stack1) == 0:
-            print(1)
-        else:
-            print(0)
-        return
-    elif commend[0] == "back":
-        if len(stack1)==0:
-            print(-1)
-        else:
-            print(stack1[0])
-        return
-    elif commend[0] == "front":
-        if len(stack1)==0:
-            print(-1)
-        else:
-            print(stack1[-1])
-        return
-
-a = int(input())
-
-for i in range(a):
-    b = list(map(str,input().split()))
-    stack_util(b)
+nge = []
+temp = []
+for i in range(n-1):
+    k = arr1[i]
+    for j in range(i+1,n):
+        if arr1[j] > k :
+            temp.append(arr1[j])
+            
+    if len(temp)==0:
+        nge.append(-1)
+    else :
+        nge.append(temp[0])
+        temp.clear()
+         
+nge.append(-1)
+print(nge)
