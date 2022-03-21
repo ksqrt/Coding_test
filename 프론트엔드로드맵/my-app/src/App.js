4ba2557ae2bf6a20d77bcc741718eb6c3eac8b04
ksqrt <ksqrt98@gmail.com>
2022-03-21 21:15:19 +0900

    app.js 수정

--- 프론트엔드로드맵/my-app/src/App.js
+++ 프론트엔드로드맵/my-app/src/App.js
@@ -22,6 +22,31 @@ function Header(props) {
     </header>
   );
 }
+function Create(props) {
+  return (
+    <article>
+      <h2>Create</h2>
+      <form
+        onSubmit={(event) => {
+          event.preventDefault();
+          const title = event.target.title.value;
+          const body = event.target.body.value;
+          props.onCreate(title, body);
+        }}
+      >
+        <p>
+          <input type="text" name="title" placeholder="title"></input>
+        </p>
+        <p>
+          <textarea name="body" placeholder="body"></textarea>
+        </p>
+        <p>
+          <input type="submit" value="Create"></input>
+        </p>
+      </form>
+    </article>
+  );
+}
 
 function Nav(props) {
   const lis = [];
@@ -36,7 +61,7 @@ function Nav(props) {
             props.onChangeMode(Number(t.id));
           }}
         >
-          {t.title} : {t.body}
+          {t.title}
         </a>
       </li>
     );
@@ -58,14 +83,17 @@ function Article(props) {
 }
 
 function App() {
-  const topics = [
-    { id:1, title: "html", body: "html is ..." },
-    { id:2, title: "css", body: "css is ..." },
-    { id:3, title: "js", body: "js is ..." },
-  ];
+  const [topics, setTopics] = useState([
+    { id: 1, title: "html", body: "html is ..." },
+    { id: 2, title: "css", body: "css is ..." },
+    { id: 3, title: "js", body: "js is ..." },
+  ]);
+
   const [mode, setMode] = useState("Welcome");
   const [id, setID] = useState(null);
+  const [nextid, setNextID] = useState(4);
   let content = null;
+
   if (mode === "Welcome") {
     content = <Article title="Welcome" body="Hello, WEB"></Article>;
   } else if (mode === "Read") {
@@ -79,12 +107,26 @@ function App() {
       }
     }
     content = <Article title={title} body={body}></Article>;
+  } else if (mode === "Create") {
+    content = (
+      <Create
+        onCreate={(_title, _body) => {
+          const newTopic = { id: nextid, title: _title, body: _body };
+          const newTopics = [...topics];
+          newTopics.push(newTopic);
+          setTopics(newTopics);
+          setMode("Read");
+          setID(nextid);
+          setNextID(nextid + 1);
+        }}
+      ></Create>
+    );
   }
 
   return (
     <div>
       <Header
-        title="REACT"
+        title="REACT WEB"
         onChangeMode={() => {
           alert("Header");
           setMode("Welcome");
@@ -92,9 +134,21 @@ function App() {
       ></Header>
       <Nav
         topics={topics}
-        onChangeMode={(id) => setMode("Read") + alert(id)}
+        onChangeMode={(id) => {
+          setMode("Read");
+          setID(id);
+        }}
       ></Nav>
       {content}
+      <a
+        href="/create"
+        onClick={(event) => {
+          event.preventDefault();
+          setMode("Create");
+        }}
+      >
+        CREATE
+      </a>
     </div>
   );
 }
