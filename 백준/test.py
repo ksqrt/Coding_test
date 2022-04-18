bc9521e4134a45e4aeae8046a0c9c6b89146e5bd
ksqrt <ksqrt98@gmail.com>
2022-04-18 22:35:58 +0900

    그리디

--- 백준/test.py
+++ 백준/test.py
@@ -1,24 +1,20 @@
-#1406 
-import sys
-stack_L =  list(sys.stdin.readline().strip())
-stack_R = []
+# 1931 
+
 n = int(input())
-
+arr = []
 for i in range(n):
-    temp =  sys.stdin.readline()
-    if  temp[0] == "L":
-        if len(stack_L) != 0 :
-            stack_R.append(stack_L.pop())
-    elif temp[0] == "D":
-        if len(stack_R) != 0 :
-            stack_L.append(stack_R.pop())
-    elif temp[0] == "B":
-        if len(stack_L) != 0 :
-            stack_L.pop()
-    elif temp[0] == "P":
-            stack_L.append((temp[2]))   
-            
-stack_R.reverse()
-stack_L.extend(stack_R) 
+    arr.append(list(map(int,input().split())))    
+print(arr)
+# 끝나는 시간이 작은순서 대로 정렬 
+# 덱 정렬후 
 
-print("".join(stack_L))
+stack = []
+tmp = 0
+# arr 이 빈 배열이 될때까지 반복
+while arr:
+    stack.append(arr[tmp])
+    del arr[tmp]
+    arr = list(filter(lambda x: x[0] > stack[tmp][1],arr))
+    tmp += 1
+
+print(len(stack))
