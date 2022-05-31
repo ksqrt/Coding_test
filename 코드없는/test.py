02ae3a8b6ee0f9a164ba32115c952f0c8d2e0ba0
ksqrt <ksqrt98@gmail.com>
2022-05-31 22:57:21 +0900

    test

--- 코드없는/test.py
+++ 코드없는/test.py
@@ -1,15 +1,16 @@
-nums = [1, 7, 3, 6, 5, 6]
-
-
-def findPivot(nums):
-    _sum = sum(nums)
-    _l = []
-    _r = []
-    for i in nums: 
-        _l.append(_sum - sum(nums[0:i]))
-        _r.append(_sum - sum(nums[i:len(nums)]))
-        
-        if _l[i] == _r[i]:
-            return _l[i]
-
-print(findPivot(nums))
+class Solution:
+    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
+        # 겹치는 구간파악을 위해, 먼저 정렬
+        merged = []
+        for arr in sorted(intervals, key = lambda x: x[0]):
+            # arr의 시작점이 구간 안에 있는 경우
+            if merged and arr[0] <= merged[-1][1]:
+                # arr의 끝점과 merged 끝점 중 더 긴 것을 merged 끝점에 업데이트
+                merged[-1][1] = max(merged[-1][1], arr[1])
+            else:
+                merged += arr,
+                # a = [1]
+                # b = [2,3]
+                # a += b // returns [1,2,3]
+                # a += b, // returns [1, [2,3]]. a += [b]와 동일한 결과를 리턴한다.
+        return merged
