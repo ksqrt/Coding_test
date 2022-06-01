ecdbff2867e956cf085ff96ad6bdcc241e6f4d8a
ksqrt <ksqrt98@gmail.com>
2022-06-01 17:38:59 +0900

    2sum

--- 코드없는/test.py
+++ 코드없는/test.py
@@ -1,16 +1,17 @@
-class Solution:
-    def merge(self, intervals: List[List[int]]) -> List[List[int]]:
-        # 겹치는 구간파악을 위해, 먼저 정렬
-        merged = []
-        for arr in sorted(intervals, key = lambda x: x[0]):
-            # arr의 시작점이 구간 안에 있는 경우
-            if merged and arr[0] <= merged[-1][1]:
-                # arr의 끝점과 merged 끝점 중 더 긴 것을 merged 끝점에 업데이트
-                merged[-1][1] = max(merged[-1][1], arr[1])
-            else:
-                merged += arr,
-                # a = [1]
-                # b = [2,3]
-                # a += b // returns [1,2,3]
-                # a += b, // returns [1, [2,3]]. a += [b]와 동일한 결과를 리턴한다.
-        return merged
+# 2sum 3sum 4sum
+
+nums = [1,3,7,4,8]
+target = 7
+
+nums.sort()
+
+for idx1 in range(len(nums)):
+    for idx2 in range(len(nums),idx1,-1):
+        value = nums[idx1]+nums[idx2]
+        if value == target:
+            print(idx1,idx2)
+        else : 
+            idx2 -= 1
+
+    
+    
