dd15cb3b0b7fca2566038412fcac64c0dd4eeefc
ksqrt <ksqrt98@gmail.com>
2023-10-05 19:04:08 +0900

    state 값을 변경하는 카운터 만들기

--- nextjs_test_app/app/list/page.js
+++ nextjs_test_app/app/list/page.js
@@ -1,7 +1,9 @@
+'use client'
 import Image from "next/image"
+import { useState } from "react"
 export default function List() {
   let url ="/list/detail"
-
+  
   let a = [
     {
       "id": 1,
@@ -17,10 +19,15 @@ export default function List() {
       "id": 3,
       "name": "토마토",
       "price": 40
+    },
+    {
+      "id": 4,
+      "name": "파인애플",
+      "price": 50
     }
   ]
-
-//   console.log(a[1])
+  
+  const [수량,수량변경] = useState(Array(a.length).fill(0))
 
   return (
        <div>
@@ -33,6 +40,23 @@ export default function List() {
                 <img src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaDjzXMdJ1hmP2M0E9zDzPJ02qpPx2aZICpV4J2Cq75OPTS4elDTjTQyu7ELPCcKoJCwmm8Gu9kkXR1djnMeOp8kfEO4-w=w100-h100" alt ={index.name}></img>
                 {/* <Image src="https://lh3.googleusercontent.com/u/0/drive-viewer/AK7aPaDjzXMdJ1hmP2M0E9zDzPJ02qpPx2aZICpV4J2Cq75OPTS4elDTjTQyu7ELPCcKoJCwmm8Gu9kkXR1djnMeOp8kfEO4-w=w100-h100" width={100} height={100}></Image> */}
                 <h4>{product.name} {product.price}원</h4>
+                <span>{수량[index]} 개  
+                <button onClick={()=>{
+                  // 1. 일단 기존 수량 복사
+                  let copy = [...수량]
+                  // 2. 해당 인덱스의 수량만 ++
+                  copy[index]++
+                  // 3. 바뀐 수량전체를 다시변경 수정변경함수 안에는 배열이 와야해서 이런식으로 해야댐 
+                  수량변경(copy)
+                }}>+</button>
+                <button onClick={()=>{
+                  // 1. 일단 기존 수량 복사
+                  let copy = [...수량]
+                  // 2. 해당 인덱스의 수량만 ++
+                  copy[index]--
+                  // 3. 바뀐 수량전체를 다시변경 수정변경함수 안에는 배열이 와야해서 이런식으로 해야댐 
+                  수량변경(copy)}}>-</button>
+                </span>
                 </div>
             ))}
             </div>
