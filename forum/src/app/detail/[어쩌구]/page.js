bbf9ecf9d659dd9f8c80d32057f76cc0764ce0fa
ksqrt <ksqrt98@gmail.com>
2023-10-06 22:49:00 +0900

    다이나믹 라우팅

---
+++ forum/src/app/detail/[어쩌구]/page.js
@@ -0,0 +1,28 @@
+import { ObjectId } from 'bson';
+import {connectDB} from '../../../util/database'
+import React from 'react';
+
+async function page(props) {
+
+  const client = await connectDB;
+  const db = client.db("forum")
+  let data = await db.collection('post').findOne({ _id : new ObjectId(props.params.어쩌구)})
+  console.log(data)
+  console.log(props)
+
+  return (
+    <div>
+      {  
+                <div className="list-bg" key={data._id}>
+                    <div className="list-item">
+                        <h4> {data.title}</h4>
+                        <p> {data.content}</p>
+                    </div>
+                </div>
+               
+        } 
+    </div>
+  );
+}
+
+export default page;
--- forum/src/app/list/page.js
+++ forum/src/app/list/page.js
@@ -1,12 +1,13 @@
 import { connectDB } from "../../util/database.js"
 
-const client = await connectDB;
-
-  const db = client.db("forum")
-  let data = await db.collection('post').find().toArray() 
-  console.log(data)
 
 export default async function List() {
+    const client = await connectDB;
+    const db = client.db("forum")
+    let data = await db.collection('post').find().toArray() 
+    console.log(data)
+
+
     return (
         <div>
         {  
@@ -14,7 +15,7 @@ export default async function List() {
                 // map 반복문 사용시 키 값에 유니크 값 넣어주면 좋아요
                 <div className="list-bg" key={index}>
                     <div className="list-item">
-                        <h4> {product.title}</h4>
+                        <h4> <a href={`/detail/${product._id}`}>{product.title}</a></h4>
                         <p> {product.content}</p>
                     </div>
                 </div>
